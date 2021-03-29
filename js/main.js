12f671027115944f5925fcdd814e0e03ebef5801
Vitya23 <mr.vityakotov@yandex.ru>
2021-03-30 00:53:46 +0300

    HTML
    HTML

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
    /* Портфолио */
-   $('.button[filter]').click(function () {
+   $('.button2[filter]').click(function () {
       if ($(this).attr('val') == 'off') {
-         $('.button[filter]').attr('val', 'off').removeClass('focused');
+         $('.button2[filter]').attr('val', 'off').removeClass('focused');
          $(this).attr('val', 'on').addClass('focused');
          $('.filter > div').hide(300);
          $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
          if ($(this).attr('filter') == 'all') {
-            $('.button[filter]').attr('val', 'off').removeClass('focused');
+            $('.button2[filter]').attr('val', 'off').removeClass('focused');
             $(this).attr('val', 'on').addClass('focused');
             $('.filter > div').show(300);
          }
